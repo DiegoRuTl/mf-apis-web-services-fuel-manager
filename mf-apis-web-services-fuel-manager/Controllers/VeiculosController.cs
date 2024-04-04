fbd39ccbccd152c16660ae941531d1f8df251da4
DiegoRuTl <you@example.com>
2024-04-04 17:05:43 -0300

    Criação do Controller Consumos e Adição dos Links de Referências

--- mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
+++ mf-apis-web-services-fuel-manager/Controllers/VeiculosController.cs
@@ -40,13 +40,15 @@ namespace mf_apis_web_services_fuel_manager.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult> GetById(int id)
         {
-            var model = await _context.Veiculos.FirstOrDefaultAsync(c => c.Id == id);
+            var model = await _context.Veiculos
+                .Include(t => t.Consumos)
+                .FirstOrDefaultAsync(c => c.Id == id);
 
             if (model == null)
             {
                 return NotFound();
             }
-
+            GerarLinks(model);
             return Ok(model);
         }
 
@@ -74,6 +76,13 @@ namespace mf_apis_web_services_fuel_manager.Controllers
             await _context.SaveChangesAsync();
 
             return NoContent();
+        }
+
+        private void GerarLinks(Veiculo model)
+        {
+            model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "self", metodo: "GET"));
+            model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "update", metodo: "PUT"));
+            model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "delete", metodo: "Delete"));
         }
 
     }
